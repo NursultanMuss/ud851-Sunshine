69114b7aa511bc3bc566d16d5459ef359e68385f
nursalga <nursalga@gmail.com>
2017-11-22 00:26:42 +0600

    22.11.2017 Нужно разобраться в RecyclerView

---
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
@@ -0,0 +1,13 @@
+package com.example.android.sunshine.utilities;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Home_PC on 22.11.2017.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+    private String[] mWeatherData;
+
+}
