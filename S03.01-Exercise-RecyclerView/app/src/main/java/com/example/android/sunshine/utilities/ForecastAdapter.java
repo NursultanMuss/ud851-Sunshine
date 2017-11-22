3e8be6ca08d37784a014d0bce90cfe35b40fbe26
nursalga <nursalga@gmail.com>
2017-11-22 08:14:16 +0600

    Утрение изменения

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
@@ -1,6 +1,10 @@
 package com.example.android.sunshine.utilities;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Home_PC on 22.11.2017.
@@ -8,6 +12,14 @@ import android.support.v7.widget.RecyclerView;
 
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
 
+    private class  ForecastAdapterViewHolder extends RecyclerView.ViewHolder{
+        public final TextView mWeatherTextView;
+
+        ForecastAdapterViewHolder(View view){
+            super(view);
+            mWeatherTextView = (TextView) view.findViewById(R.)
+        }
+    }
     private String[] mWeatherData;
 
 }
