2e4a5d99db678bb7b3138701021590b35dc605b3
nursalga <nursalga@gmail.com>
2017-11-29 00:11:52 +0600

    Что то сделал

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/utilities/ForecastAdapter.java
@@ -1,8 +1,13 @@
 package com.example.android.sunshine.utilities;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.example.android.sunshine.R;
 
 import org.w3c.dom.Text;
 
@@ -17,9 +22,39 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
         ForecastAdapterViewHolder(View view){
             super(view);
-            mWeatherTextView = (TextView) view.findViewById(R.)
+            mWeatherTextView = (TextView) view.findViewById(R.id.tv_weather_data);
         }
     }
     private String[] mWeatherData;
 
+    @Override
+    public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        int layoutIdForListItem =R.layout.forecast_list_item;
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem,parent,shouldAttachToParentImmediately);
+
+        ForecastAdapterViewHolder viewHolder = new ForecastAdapterViewHolder( view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapterViewHolder holder, int position) {
+        holder.mWeatherTextView.setText(String.valueOf(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mWeatherData == null){
+            return 0;
+        }else return mWeatherData.length;
+    }
+
+    private setWeatherData (){
+        mWeatherData = new String[1];
+
+    }
 }
